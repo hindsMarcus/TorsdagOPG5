688e186692e94d591f5e764dc29e4e3b52db54eb
Marcus <marcushindsboel@gmail.com>
2022-03-07 14:11:42 +0100

    Further work

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main
@@ -26,9 +27,13 @@ public class Main
 
         printWordsOfLength(3);
 
-
         //test dine metoder ved at kalde dem her:
 
+        printLongestWord();
+
+        printFirstHalfOfEachWord();
+
+        printMostFrequentLetter(text, myChar );
 
     }
 
@@ -66,5 +71,35 @@ public class Main
 
     //skriv dine metoder herunder:
 
+    public static String printLongestWord() throws FileNotFoundException {
+        String longestWord = "";
+        String current;
+        Scanner scan = new Scanner(new File("data.txt"));
 
+
+        while (scan.hasNext()) {
+            current = scan.next();
+            if (current.length() > longestWord.length()) {
+                longestWord = current;
+            }
+
+        }
+        System.out.println("\n"+longestWord+"\n");
+        return longestWord;
+    }
+
+    public static void printFirstHalfOfEachWord() {
+
+        for (String s : text) {
+            System.out.println(s.substring(0, s.length() / 2));
+        }
+    }
+
+    public static void printMostFrequentLetter(String fileName, Map<String, Integer> chars) throws FileNotFoundException {
+
+    }
+
+    public static void printLeastFrequentLetter() {
+        
+    }
 }
